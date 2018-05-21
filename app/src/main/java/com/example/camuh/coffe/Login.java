a57b81f6f1c97910a70fb9e37b0340a16bf36228
hanafikambivi <hanafikambivi1200@gmail.com>
2018-05-21 15:55:19 +0800

    hanafi
    Lanjutan Login

--- app/src/main/java/com/example/camuh/coffe/Login.java
+++ app/src/main/java/com/example/camuh/coffe/Login.java
@@ -2,12 +2,28 @@ package com.example.camuh.coffe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class Login extends AppCompatActivity {
-
+    @BindView(R.id.user) EditText user;
+    @BindView(R.id.pass) EditText pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+    @OnClick(R.id.login)
+    public void loginMasuk() {
+        int user, pass, login;
+        user = Integer.valueOf(user.getText().toString())
+        //Method onClick pada Button
+
+        EditText = (EditText)findViewById(R.id.user);
+        EditText = (EditText) findViewById(R.id.pass);
+        text EditText.getText().toString();
+        text = EditText.getText().toString();
     }
 }
